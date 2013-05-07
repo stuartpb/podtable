0cd82b1a7b2b607f697867c8d3f34aac52428484
Stuart P. Bentley <stuart@testtrack4.com>
2013-05-07 02:57:21 -0400

    Add first take on animation generation

--- podtable.js
+++ podtable.js
@@ -30,6 +30,43 @@ function getYAML(url,callback) {
   req.send();
 }
 
+function svgElem(tag){
+  return document.createElementNS(tag,"http://www.w3.org/2000/svg");
+}
+
+function svgTime(time){
+  /*global ms*/
+  if(typeof time == 'string'){
+    return ms(ms(time));
+  } else return ms(time);
+}
+
+//TODO: Make this constant from run to run
+function randomBool(){
+  return Math.random() > 0.5;
+}
+function randomVal(low,high){
+  return Math.random() * (high-low) + low;
+}
+
+function randomStartEnd(edges){
+  var coords = [[],[]];
+  //Choose random axis and direction
+  var crossAxis = +randomBool(); var otherAxis = +!crossAxis;
+  var startEdge = +randomBool(); var endEdge = +!startEdge;
+  
+  //Set edges for the edge-to-edge axis
+  coords[otherAxis][startEdge] = edges[otherAxis][startEdge];
+  coords[otherAxis][endEdge] = edges[otherAxis][endEdge];
+  
+  //Set start and opposite end for the intermediate axis
+  var startCoord = randomVal(coords[crossAxis][startEdge],
+    coords[crossAxis][endEdge]);
+  coords[crossAxis][startEdge] = startCoord;
+  coords[crossAxis][endEdge] = coords[crossAxis][endEdge] - startCoord;
+  return coords;
+}
+
 //Start paused
 document.getElementById("podcast-animation").setCurrentTime(0);
 document.getElementById("podcast-animation").pauseAnimations();
@@ -55,4 +92,36 @@ getYAML('episode.yaml',function(episode){
   //todo: handle multiple sources
   document.getElementById("podcast-audio").src = asset(episode.audio);
   
+  var stage = document.getElementById("podcast-animation");
+  var stageW, stageH;
+  if(episode.background){
+    stageW = episode.background.width;
+    stageH = episode.background.height;
+    var bg = svgElem('image');
+    bg.href = asset(episode.background.image);
+    bg.setAttribute('width',stageW);
+    bg.setAttribute('height',stageH);
+    stage.appendChild(bg);
+  } else {
+    stageW = 640; stageH = 480;
+  }
+  
+  var baseSize = Math.min(stageW, stageH) / 3;
+  
+  var edges = [[-baseSize,stageW+baseSize],[-baseSize,stageH+baseSize]];
+  
+  for(var i = 0; i < episode.images; i++) {
+    var epImage = episode.images[i];
+    var image = svgElem('image');
+    image.setAttribute('width',baseSize);
+    image.setAttribute('height',baseSize);
+    var anim = svgElem('animateMotion');
+    anim.setAttribute('begin', svgTime(epImage.begin));
+    anim.setAttribute('end', svgTime(epImage.end));
+    var rseCoords = randomStartEnd(edges);
+    anim.setAttribute('from', rseCoords[0].join());
+    anim.setAttribute('to', rseCoords[1].join());
+    image.appendChild(anim);
+    stage.appendChild(image);
+  }
 });
